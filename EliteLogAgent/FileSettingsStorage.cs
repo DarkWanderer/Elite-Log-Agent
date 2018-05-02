a975ec2090792bfc063c04039deff4b92904309c
DarkWanderer <elquentaro@gmail.com>
2018-05-03 00:14:29 +0300

    Added settings saving

--- EliteLogAgent/FileSettingsStorage.cs
+++ EliteLogAgent/FileSettingsStorage.cs
@@ -1,18 +1,44 @@
 ﻿using Interfaces;
 using Interfaces.Settings;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using NLog;
+using System;
+using System.IO;
 
 namespace EliteLogAgent
 {
     public class FileSettingsStorage : ISettingsProvider
     {
+        private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
         public GlobalSettings Settings
         {
             get
             {
-                return new GlobalSettings();
+                try
+                {
+                    return JsonConvert.DeserializeObject<GlobalSettings>(File.ReadAllText(SettingsFilePath));
+                }
+                catch (Exception e)
+                {
+                    Log.Warn(e, "Exception while reading settings, using defaults");
+                    return new GlobalSettings();
+                }
             }
-            set { }
+            set
+            {
+                Directory.CreateDirectory(SettingsFileDirectory);
+
+                using (FileStream fileStream = File.Open(SettingsFilePath, FileMode.Create))
+                using (StreamWriter streamWriter = new StreamWriter(fileStream))
+                using (JsonWriter jsonWriter = new JsonTextWriter(streamWriter))
+                {
+                    jsonWriter.Formatting = Formatting.Indented;
+                    JsonSerializer serializer = new JsonSerializer();
+                    serializer.Serialize(jsonWriter, value);
+                }
+            }
         }
 
         public JObject GetPluginSettings(string plugin)
@@ -21,5 +47,8 @@ namespace EliteLogAgent
             Settings.PluginSettings.TryGetValue(plugin, out result);
             return result;
         }
+
+        private string SettingsFileDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "EliteLogAgent");
+        private string SettingsFilePath => Path.Combine(SettingsFileDirectory, "Settings.json");
     }
 }
