343747d9b30909852b5d1e91edd6017b4317c3e7
DarkWanderer <elquentaro@gmail.com>
2018-08-08 00:42:59 +0300

    Added plugin skeleton

---
+++ DW.ELA.Plugin.Canonn/CanonnPlugin.cs
@@ -0,0 +1,25 @@
+﻿using DW.ELA.Controller;
+using DW.ELA.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DW.ELA.Plugin.Canonn
+{
+    public class CanonnPlugin : AbstractPlugin<object, CanonnApiSettings>
+    {
+        protected CanonnPlugin(ISettingsProvider settingsProvider) : base(settingsProvider)
+        {
+        }
+
+        public override string PluginName => "Canonn";
+        public override string PluginId => "Canonn";
+        protected override IEventConverter<object> EventConverter { get; }
+
+        public override void FlushEvents(ICollection<object> events) => throw new NotImplementedException();
+        public override AbstractSettingsControl GetPluginSettingsControl(ELA.Interfaces.Settings.GlobalSettings settings) => throw new NotImplementedException();
+        public override void ReloadSettings() => throw new NotImplementedException();
+    }
+}
