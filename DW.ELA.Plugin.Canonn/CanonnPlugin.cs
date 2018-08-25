a36e1291ddc611d5f54b2c5a544b5bea5026949a
DarkWanderer <elquentaro@gmail.com>
2018-08-25 17:56:03 +0300

    Enabled Canonn plugin

--- DW.ELA.Plugin.Canonn/CanonnPlugin.cs
+++ DW.ELA.Plugin.Canonn/CanonnPlugin.cs
@@ -1,5 +1,6 @@
 ﻿using DW.ELA.Controller;
 using DW.ELA.Interfaces;
+using DW.ELA.Interfaces.Settings;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,16 +11,14 @@ namespace DW.ELA.Plugin.Canonn
 {
     public class CanonnPlugin : AbstractPlugin<object, CanonnApiSettings>
     {
-        protected CanonnPlugin(ISettingsProvider settingsProvider) : base(settingsProvider)
-        {
-        }
+        public CanonnPlugin(ISettingsProvider settingsProvider) : base(settingsProvider) { }
 
         public override string PluginName => "Canonn";
         public override string PluginId => "Canonn";
         protected override IEventConverter<object> EventConverter { get; }
 
         public override void FlushEvents(ICollection<object> events) => throw new NotImplementedException();
-        public override AbstractSettingsControl GetPluginSettingsControl(ELA.Interfaces.Settings.GlobalSettings settings) => throw new NotImplementedException();
+        public override AbstractSettingsControl GetPluginSettingsControl(GlobalSettings settings) => throw new NotImplementedException();
         public override void ReloadSettings() => throw new NotImplementedException();
     }
 }
