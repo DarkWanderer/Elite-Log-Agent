960d1e18aad243c228fea4d797a4cfe7baf8eef5
DarkWanderer <elquentaro@gmail.com>
2018-09-15 00:10:36 +0300

    Avoid sending same EDDN messages over and over again (board flipping) #32

--- DW.ELA.Plugin.EDDN/EddnPlugin.cs
+++ DW.ELA.Plugin.EDDN/EddnPlugin.cs
@@ -2,15 +2,17 @@
 using DW.ELA.Interfaces;
 using DW.ELA.Interfaces.Settings;
 using DW.ELA.Plugin.EDDN.Model;
+using Newtonsoft.Json.Linq;
 using NLog;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using Utility;
 
 namespace DW.ELA.Plugin.EDDN
 {
-    public class EddnPlugin : AbstractPlugin<EddnEvent,EddnSettings>
+    public class EddnPlugin : AbstractPlugin<EddnEvent, EddnSettings>
     {
         private static readonly IRestClient RestClient = new ThrottlingRestClient("https://eddn.edcd.io:4430/upload/");
         private static readonly ILogger logger = LogManager.GetCurrentClassLogger();
@@ -35,15 +37,49 @@ namespace DW.ELA.Plugin.EDDN
         public override string PluginName => "EDDN";
         public override string PluginId => "EDDN";
         // EDDN accepts events one-by-one, so no need to batch
-        public override TimeSpan FlushInterval => TimeSpan.FromSeconds(1); 
+        public override TimeSpan FlushInterval => TimeSpan.FromSeconds(1);
 
         public override AbstractSettingsControl GetPluginSettingsControl(GlobalSettings settings) => null;
         public override async void FlushEvents(ICollection<EddnEvent> events)
         {
             foreach (var @event in events)
                 schemaManager.ValidateSchema(@event);
-            await apiFacade.PostEventsAsync(events.ToArray());
+            await apiFacade.PostEventsAsync(events.Where(IsUnique).ToArray());
         }
+
+        private readonly ConcurrentQueue<JObject> lastPushedEvents = new ConcurrentQueue<JObject>(); // stores
+
+        /// <summary>
+        /// Check event against list of last few sent events, excluding timestamp from comparison
+        /// </summary>
+        /// <param name="e"></param>
+        /// <returns>true if event wasn't sent before</returns>
+        private bool IsUnique(EddnEvent e)
+        {
+            try
+            {
+                JObject jObject;
+                while (lastPushedEvents.Count > 10)
+                    lastPushedEvents.TryDequeue(out jObject);
+
+                jObject = JObject.FromObject(e);
+                var messageObject = jObject.Property("message")?.Value as JObject;
+                messageObject?.Property("timestamp")?.Remove();
+
+                foreach (var recent in lastPushedEvents)
+                    if (JToken.DeepEquals(jObject,recent))
+                        return false;
+
+                lastPushedEvents.Enqueue(jObject);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Error in deduplication");
+                return true; // by default, we consider any message unique and so will send it
+            }
+        }
+
 
         public override void ReloadSettings() => eventConverter.UploaderID = settingsProvider.Settings.CommanderName;
     }
