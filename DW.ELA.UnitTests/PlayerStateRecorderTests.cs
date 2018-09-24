13c6853ef0f01cf0219f456774f8ecead78c0a74
DarkWanderer <elquentaro@gmail.com>
2018-09-24 23:39:29 +0300

    Added Fighter/SRV tests for PlayerStateRecorder

--- DW.ELA.UnitTests/PlayerStateRecorderTests.cs
+++ DW.ELA.UnitTests/PlayerStateRecorderTests.cs
@@ -1,5 +1,6 @@
 ﻿using Controller;
 using DW.ELA.Interfaces;
+using DW.ELA.Interfaces.Events;
 using NUnit.Framework;
 using System;
 
@@ -8,7 +9,6 @@ namespace DW.ELA.UnitTests
     [TestFixture]
     public class PlayerStateRecorderTests
     {
-
         private readonly IPlayerStateHistoryRecorder eventConverter = new PlayerStateRecorder();
 
         [Test]
@@ -21,6 +21,30 @@ namespace DW.ELA.UnitTests
             eventConverter.GetPlayerShipType(DateTime.UtcNow);
             eventConverter.GetPlayerShipId(DateTime.UtcNow);
             eventConverter.GetPlayerIsInCrew(DateTime.UtcNow);
+        }
+
+        private const string ValidShip = "Valid_Ship";
+        private const long ValidShipId = 77;
+
+        [Test]
+        public void ShouldNotRecordFighterOrSrv()
+        {
+            var time1 = DateTime.UtcNow;
+            var time2 = time1.AddSeconds(5);
+            var time3 = time2.AddSeconds(5);
+            var time4 = time3.AddSeconds(5);
+
+            eventConverter.OnNext(new LoadGame() { Timestamp = time1, Ship = ValidShip, ShipId = ValidShipId });
+            Assert.AreEqual(ValidShip, eventConverter.GetPlayerShipType(time4));
+            Assert.AreEqual(ValidShipId, eventConverter.GetPlayerShipId(time4));
+
+            eventConverter.OnNext(new LoadGame() { Timestamp = time2, Ship = "Some_Fighter", ShipId = 567 });
+            Assert.AreEqual(ValidShip, eventConverter.GetPlayerShipType(time4));
+            Assert.AreEqual(ValidShipId, eventConverter.GetPlayerShipId(time4));
+
+            eventConverter.OnNext(new LoadGame() { Timestamp = time3, Ship = "testbuggy", ShipId = 123 });
+            Assert.AreEqual(ValidShip, eventConverter.GetPlayerShipType(time4));
+            Assert.AreEqual(ValidShipId, eventConverter.GetPlayerShipId(time4));
         }
     }
 }
