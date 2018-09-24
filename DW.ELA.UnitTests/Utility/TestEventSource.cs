c5a4905bedf996de26ec5aef70e7c04095b27916
DarkWanderer <elquentaro@gmail.com>
2018-09-24 23:23:21 +0300

    Reverted TestEventSource to previous version

--- DW.ELA.UnitTests/Utility/TestEventSource.cs
+++ DW.ELA.UnitTests/Utility/TestEventSource.cs
@@ -4,30 +4,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.IO;
-using System;
 
 namespace DW.ELA.UnitTests
 {
     public static class TestEventSource
     {
-        public static IEnumerable<LogEvent> LogEvents => lazyEvents.Value;
-        public static IEnumerable<LogEvent> TypedLogEvents => lazyEvents.Value.Where(e => e.GetType() != typeof(LogEvent));
+        public static IEnumerable<LogEvent> LogEvents => GetLogEvents();
+        public static IEnumerable<LogEvent> TypedLogEvents => GetLogEvents().Where(e => e.GetType() != typeof(LogEvent));
 
-        private static readonly Lazy<IReadOnlyCollection<LogEvent>> lazyEvents = new Lazy<IReadOnlyCollection<LogEvent>>(LoadEvents);
-
-        private static IReadOnlyCollection<LogEvent> LoadEvents()
+        private static IEnumerable<LogEvent> GetLogEvents()
         {
             var assembly = Assembly.GetExecutingAssembly();
             var resourceName = "DW.ELA.UnitTests.CannedEvents.json";
-            var events = new List<LogEvent>();
 
             using (var stream = assembly.GetManifestResourceStream(resourceName))
             using (var textReader = new StreamReader(stream))
             {
                 var reader = new LogReader();
-                events.AddRange(reader.ReadEventsFromStream(textReader));
+                foreach (var @event in reader.ReadEventsFromStream(textReader))
+                    yield return @event;
             }
-            return events;
         }
     }
 }
