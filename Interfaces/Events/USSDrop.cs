f7a0b81a4cd8e3ecad98f71fb9f18f02bd60c111
DarkWanderer <elquentaro@gmail.com>
2018-10-04 22:37:26 +0300

    Added USSDrop event, made actual plugins IObserver instead of IPlugin interface, added try/catch on SettingsForm init

---
+++ Interfaces/Events/USSDrop.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace DW.ELA.Interfaces.Events
+{
+    public class USSDrop : LogEvent
+    {
+        [JsonProperty("USSType")]
+        public string UssType { get; set; }
+
+        [JsonProperty("USSType_Localised")]
+        public string UssTypeLocalised { get; set; }
+
+        [JsonProperty("USSThreat")]
+        public int UssThreat { get; set; }
+    }
+}
