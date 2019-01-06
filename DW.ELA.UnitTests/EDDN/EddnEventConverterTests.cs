b12a8cce700e643cd772019773469efe1e80ad14
DarkWanderer <elquentaro@gmail.com>
2019-01-06 15:53:45 +0300

    Added test to verify enrichment works correctly

--- DW.ELA.UnitTests/EDDN/EddnEventConverterTests.cs
+++ DW.ELA.UnitTests/EDDN/EddnEventConverterTests.cs
@@ -2,7 +2,9 @@
 {
     using System;
     using System.Linq;
+    using DW.ELA.Controller;
     using DW.ELA.Interfaces;
+    using DW.ELA.Interfaces.Events;
     using DW.ELA.Plugin.EDDN;
     using DW.ELA.Plugin.EDDN.Model;
     using Moq;
@@ -10,23 +12,54 @@
 
     [TestFixture]
     public class EddnEventConverterTests
-    {
+    { 
         [Test]
         [Parallelizable]
         [TestCaseSource(typeof(TestEventSource), nameof(TestEventSource.CannedEvents))]
         public void ShouldNotFailOnEvents(LogEvent e)
         {
-            var mockRecorder = new Mock<IPlayerStateHistoryRecorder>();
-            mockRecorder.Setup(r => r.GetStarPos(It.IsAny<string>())).Returns(new[] { 0.0, 1.1, 2.2 });
-            mockRecorder.Setup(r => r.GetPlayerSystem(It.IsAny<DateTime>())).Returns("SomeSystem");
+            var recorderMock = GetRecorderMock();
 
-            var eventConverter = new EddnEventConverter(mockRecorder.Object) { MaxAge = TimeSpan.FromDays(5000) };
+            var eventConverter = new EddnEventConverter(recorderMock) { MaxAge = TimeSpan.FromDays(5000) };
             var result = eventConverter.Convert(e).ToList();
             Assert.NotNull(result);
             CollectionAssert.AllItemsAreInstancesOfType(result, typeof(EddnEvent));
             var validator = new EventSchemaValidator();
             foreach (var @event in result)
                 Assert.IsTrue(validator.ValidateSchema(@event), "Event {0} should have validated", e.Event);
+        }
+
+        [Test]
+        [Parallelizable]
+        public void ShouldEnrichJournalEventsWithStarSystemFields()
+        {
+            var recorderMock = GetRecorderMock();
+
+            var eventConverter = new EddnEventConverter(recorderMock) { MaxAge = TimeSpan.FromDays(5000) };
+
+            var convertedEvents = TestEventSource.CannedEvents
+                .SelectMany(eventConverter.Convert)
+                .OfType<JournalEvent>()
+                .ToList();
+
+            CollectionAssert.IsNotEmpty(convertedEvents);
+            CollectionAssert.AllItemsAreNotNull(convertedEvents);
+
+            foreach (var e in convertedEvents.OfType<JournalEvent>())
+            {
+                Assert.NotNull(e.Message.Property("SystemAddress"));
+                Assert.NotNull(e.Message.Property("StarPos"));
+                Assert.NotNull(e.Message.Property("StarSystem"));
+            }
+        }
+
+        private IPlayerStateHistoryRecorder GetRecorderMock()
+        {
+            var recorderMock = new Mock<IPlayerStateHistoryRecorder>();
+            recorderMock.Setup(r => r.GetStarPos(It.IsAny<string>())).Returns(new[] { 0.0, 1.1, 2.2 });
+            recorderMock.Setup(r => r.GetPlayerSystem(It.IsAny<DateTime>())).Returns("SomeSystem");
+            recorderMock.Setup(r => r.GetSystemAddress(It.IsAny<string>())).Returns(123456789456);
+            return recorderMock.Object;
         }
     }
 }
