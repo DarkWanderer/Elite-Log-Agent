b85f38416f2e56275e615a75ee42a9b8ca449181
DarkWanderer <elquentaro@gmail.com>
2019-02-13 00:25:30 +0300

    Added support for Inara mission effects

--- DW.ELA.UnitTests/INARA/InaraIntegrationTests.cs
+++ DW.ELA.UnitTests/INARA/InaraIntegrationTests.cs
@@ -1,5 +1,6 @@
 ﻿namespace DW.ELA.UnitTests.INARA
 {
+    using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
     using DW.ELA.Controller;
@@ -12,6 +13,13 @@
     [TestFixture]
     public class InaraIntegrationTests
     {
+        private readonly IReadOnlyCollection<string> ignoredErrors = new HashSet<string>
+        {
+            "Everything was alright, the near-neutral status just wasn't stored.",
+            "There is a newer inventory state recorded already.",
+            "This ship was not found but was added automatically."
+        };
+
         [Test]
         [Explicit]
         public async Task IntegrationTestUploadToInara()
@@ -36,8 +44,7 @@
             var results = await inaraApiFacade.ApiCall(convertedEvents);
             results = results
                 .Where(e => e.EventStatus != 200)
-                .Where(e => e.EventStatusText != "There is a newer inventory state recorded already.")
-                .Where(e => e.EventStatusText != "This ship was not found but was added automatically.")
+                .Where(e => !ignoredErrors.Contains(e.EventStatusText))
                 .ToList();
 
             CollectionAssert.IsEmpty(results);
