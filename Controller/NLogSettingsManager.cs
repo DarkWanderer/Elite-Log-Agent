a7eeeb4fe0d5fb01051d704e598a3ec19050de63
DarkWanderer <elquentaro@gmail.com>
2019-03-06 21:12:27 +0300

    Timestamp format fix

--- Controller/NLogSettingsManager.cs
+++ Controller/NLogSettingsManager.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.IO;
+    using System.Linq;
     using System.Text;
     using DW.ELA.Interfaces;
     using DW.ELA.Utility;
@@ -120,8 +121,10 @@
         private JsonLayout GetCloudErrorLayout()
         {
             var layout = GetDefaultJsonLayout();
+            layout.Attributes.Remove(layout.Attributes.Single(a => a.Name == "time"));
             layout.Attributes.Add(new JsonAttribute("application", AppInfo.Name));
             layout.Attributes.Add(new JsonAttribute("version", AppInfo.Version));
+            layout.Attributes.Add(new JsonAttribute("time", "${date:format=o}"));
             return layout;
         }
 #pragma warning restore SA1118 // Parameter must not span multiple lines
