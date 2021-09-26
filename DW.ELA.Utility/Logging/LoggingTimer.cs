742b10aa0380c57681b36f4c318f017ef2f435c8
DarkWanderer <elquentaro@gmail.com>
2021-09-26 21:43:06 +0300

    Improved LoggingTimer

--- DW.ELA.Utility/Logging/LoggingTimer.cs
+++ DW.ELA.Utility/Logging/LoggingTimer.cs
@@ -7,24 +7,33 @@ namespace DW.ELA.Utility.Log
 {
     public class LoggingTimer : IDisposable
     {
-        private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+        private static readonly Logger DefaultLogger = LogManager.GetCurrentClassLogger();
+        private static readonly LogLevel DefaultLogLevel = LogLevel.Info;
+
         private readonly Stopwatch stopwatch = new();
+        private readonly Logger logger;
+        private readonly LogLevel logLevel;
         private readonly string context;
 
-        public LoggingTimer(string context)
-        {
-            this.context = context;
-            stopwatch.Start();
-        }
+        public LoggingTimer(string context) : this(context, LogLevel.Info, DefaultLogger) { }
 
-        public LogLevel LogLevel { get; set; } = LogLevel.Debug;
+        public LoggingTimer(string context, LogLevel logLevel) : this(context, logLevel, DefaultLogger) { }
+
+        public LoggingTimer(string context, Logger logger) : this(context, DefaultLogLevel, logger) { }
+
+        public LoggingTimer(string context, LogLevel logLevel, Logger logger)
+        {
+            this.context = context ?? throw new ArgumentNullException(nameof(context));
+            this.logger = logger;
+            this.logLevel = logLevel ?? throw new ArgumentNullException(nameof(logLevel));
+        }
 
         public TimeSpan Elapsed => stopwatch.Elapsed;
 
         public void Dispose()
         {
             stopwatch.Stop();
-            Log.Log(LogLevel)
+            logger.Log(logLevel)
                 .Message("{0}", context)
                 .Property("duration", stopwatch.ElapsedMilliseconds)
                 .Write();
